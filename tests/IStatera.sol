dde1e20f33e5244a564d40798d16f91f7e9e73b0
Michal Převrátil <michprev@gmail.com>
2024-03-07 10:52:47 +0100

    🚧 Fuzz test update

---
+++ tests/IStatera.sol
@@ -0,0 +1,4 @@
+
+interface IStatera {
+    function cut(uint256 value) external view returns (uint256);
+}
--- tests/test_fuzz_v2.py
+++ tests/test_fuzz_v2.py
@@ -1,7 +1,7 @@
 import logging
 import random
 from collections import defaultdict
-from typing import Dict, NamedTuple
+from typing import Callable, Dict, NamedTuple
 from wake.testing import *
 from wake.testing.fuzzing import *
 from pytypes.axelarnetwork.axelargmpsdksolidity.contracts.interfaces.IAxelarExecutable import IAxelarExecutable
@@ -15,10 +15,35 @@ from pytypes.sourcev2.contracts.TokenHandler import TokenHandler
 from pytypes.sourcev2.contracts.interchaintoken.InterchainToken import InterchainToken
 from pytypes.sourcev2.contracts.interfaces.IERC20Named import IERC20Named
 from pytypes.sourcev2.contracts.proxies.InterchainProxy import InterchainProxy
+from pytypes.sourcev2.contracts.proxies.TokenManagerProxy import TokenManagerProxy
 from pytypes.sourcev2.contracts.tokenmanager.TokenManager import TokenManager
 from pytypes.sourcev2.contracts.utils.InterchainTokenDeployer import InterchainTokenDeployer
 from pytypes.sourcev2.contracts.utils.TokenManagerDeployer import TokenManagerDeployer
 from pytypes.tests.Create3 import Create3
+from pytypes.tests.IStatera import IStatera
+
+from pytypes.weird.Approval import ApprovalRaceToken
+from pytypes.weird.ApprovalToZero import ApprovalToZeroToken
+from pytypes.weird.DaiPermit import DaiPermit
+from pytypes.weird.HighDecimals import HighDecimalToken
+from pytypes.weird.LowDecimals import LowDecimalToken
+from pytypes.weird.MissingReturns import MissingReturnToken
+from pytypes.weird.NoRevert import NoRevertToken
+from pytypes.weird.RevertToZero import RevertToZeroToken
+from pytypes.weird.Uint96 import Uint96ERC20
+
+
+weird_tokens: List[Callable[[int, Chain], Address]] = [
+    lambda sup, chain: ApprovalRaceToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: ApprovalToZeroToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: DaiPermit.deploy(sup, chain=chain).address,
+    lambda sup, chain: HighDecimalToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: LowDecimalToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: MissingReturnToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: NoRevertToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: RevertToZeroToken.deploy(sup, chain=chain).address,
+    lambda sup, chain: Uint96ERC20.deploy(sup, chain=chain).address,
+]
 
 
 chain1 = Chain()
@@ -33,21 +58,22 @@ gateway_tokens = [
     Address("0xB8c77482e45F1F44dE1745F52C74426C631bDD52"), # bnb
     Address("0x6b175474e89094c44da98b954eedeac495271d0f"), # dai
     Address("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"), # weth
-    #Address("0xa7DE087329BFcda5639247F96140f9DAbe3DeED1"), # sta (fee on transfer)
     Address("0x80fB784B7eD66730e8b1DBd9820aFD29931aab03"), # lend
     Address("0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a"), # yamv2
-    #Address("0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"), # mkr (symbol decoding issue)
     Address("0xdb25f211ab05b1c97d595516f45794528a807ad8"), # eurs
     Address("0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"), # uni
 ]
 
 other_tokens = [
-    #Address("0x514910771af9ca656af840dff83e8264ecf986ca"),  # link
-    #Address("0x5a98fcbea516cf06857215779fd812ca3bef1b32"),  # ldo
+    Address("0x514910771af9ca656af840dff83e8264ecf986ca"),  # link
+    #Address("0x5a98fcbea516cf06857215779fd812ca3bef1b32"), # ldo (mint issue)
     Address("0xc944e90c64b2c07662a292be6244bdf05cda44a7"),  # grt
     Address("0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"),  # aave
-    #Address("0xa7DE087329BFcda5639247F96140f9DAbe3DeED1"), # sta (fee on transfer)
     #Address("0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2"), # mkr (symbol decoding issue)
+]
+
+fee_on_transfer_tokens = [
+    Address("0xa7DE087329BFcda5639247F96140f9DAbe3DeED1"), # sta (fee on transfer)
 ]
 
 
@@ -124,12 +150,14 @@ class ItsFuzzTest(FuzzTest):
             mint_erc20(t, d.gw, 10_000_000)
 
     def _setup_balances(self, chain: Chain):
-        its = self.deployments[chain].its
+        d = self.deployments[chain]
         for token_id in self.token_ids[chain]:
-            t = IERC20(its.validTokenAddress(token_id), chain=chain)
+            t = IERC20(d.its.validTokenAddress(token_id), chain=chain)
             self.balances[t] = defaultdict(int)
             for acc in chain.accounts:
                 self.balances[t][acc] = t.balanceOf(acc)
+            self.balances[t][d.gw] = t.balanceOf(d.gw)
+            self.balances[t][d.its] = t.balanceOf(d.its)
 
     def _relay(self, tx: TransactionAbc):
         other_chain = chain1 if tx.chain == chain2 else chain2
@@ -209,9 +237,12 @@ class ItsFuzzTest(FuzzTest):
         self.token_ids[chain].append(token_id)
 
         t = IERC20(token, chain=chain)
-        mint_erc20(t, d.its.tokenManagerAddress(token_id), 10_000_000)
+        token_manager = TokenManagerProxy(d.its.tokenManagerAddress(token_id), chain=chain)
+        mint_erc20(t, token_manager, 10_000_000)
 
-        self.balances[t] = defaultdict(int)
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
+        self.balances[t][token_manager] += 10_000_000
         for acc in chain.accounts:
             self.balances[t][acc] = t.balanceOf(acc)
 
@@ -223,22 +254,13 @@ class ItsFuzzTest(FuzzTest):
 
         other_token = self.deployments[other_chain].its.validTokenAddress(token_id)
         t = InterchainToken(other_token, chain=other_chain)
-        self.balances[t] = defaultdict(int)
+
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
         for acc in other_chain.accounts:
             self.balances[t][acc] = t.balanceOf(acc)
 
         self.interchain_tokens[other_chain].append(t)
-
-        if False:
-            self.invariant_balances()
-
-            token_id2 = self.deployments[other_chain].factory.registerCanonicalInterchainToken(
-                other_token, from_=random_account(chain=other_chain)
-            ).return_value
-            assert self.deployments[other_chain].factory.deployRemoteCanonicalInterchainToken(
-                f"chain{other_chain.chain_id}", other_token, f"chain{chain.chain_id}", 0, from_=random_account(chain=other_chain)
-            ).return_value == token_id2
-            breakpoint()
 
     @flow()
     def flow_deploy_interchain_token(self):
@@ -266,11 +288,13 @@ class ItsFuzzTest(FuzzTest):
 
         self.token_ids[chain].append(token_id)
         t = IERC20(d.its.validTokenAddress(token_id), chain=chain)
-        self.balances[t] = defaultdict(int)
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
 
         self.token_ids[other_chain].append(token_id)
         t = IERC20(self.deployments[other_chain].its.validTokenAddress(token_id), chain=other_chain)
-        self.balances[t] = defaultdict(int)
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
 
         self.interchain_tokens[chain].append(
             InterchainToken(d.its.validTokenAddress(token_id), chain=chain)
@@ -278,6 +302,70 @@ class ItsFuzzTest(FuzzTest):
         self.interchain_tokens[other_chain].append(
             InterchainToken(self.deployments[other_chain].its.validTokenAddress(token_id), chain=other_chain)
         )
+
+    @flow(max_times=30)
+    def flow_deploy_token_manager(self):
+        chain = random.choice([chain1, chain2])
+        other_chain = chain1 if chain == chain2 else chain2
+        d = self.deployments[chain]
+        token_manager_type = random.choice([
+            InterchainTokenService.TokenManagerType.GATEWAY,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+        ])
+
+        if token_manager_type == InterchainTokenService.TokenManagerType.LOCK_UNLOCK:
+            if random.random() < 0.3:
+                source_token = random.choice(other_tokens)
+                dest_token = source_token
+            else:
+                total_supply = random_int(1_000_000, 10_000_000)
+                source_token = random.choice(weird_tokens)(total_supply, chain)
+                dest_token = random.choice(weird_tokens)(total_supply, other_chain)
+        elif token_manager_type == InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE:
+            source_token = random.choice(fee_on_transfer_tokens)
+            dest_token = source_token
+        else:
+            source_token = random.choice(gateway_tokens)
+            dest_token = source_token
+
+        deployer = random_account(chain=chain).address
+        salt = random_bytes(32)
+        token_id = d.its.deployTokenManager(
+            salt, "", token_manager_type, abi.encode(b"", source_token), 0, from_=deployer
+        ).return_value
+
+        assert d.its.deployTokenManager(
+            salt, f"chain{other_chain.chain_id}", token_manager_type, abi.encode(b"", dest_token), 0, from_=deployer
+        ).return_value == token_id
+
+        if token_manager_type in {
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+        }:
+            source_token_manager = TokenManagerProxy(d.its.tokenManagerAddress(token_id), chain=chain)
+            dest_token_manager = TokenManagerProxy(
+                self.deployments[other_chain].its.tokenManagerAddress(token_id), chain=other_chain
+            )
+            mint_erc20(IERC20(source_token, chain=chain), source_token_manager, 10_000_000)
+            mint_erc20(IERC20(dest_token, chain=other_chain), dest_token_manager, 10_000_000)
+
+        self.token_ids[chain].append(token_id)
+        t = IERC20(d.its.validTokenAddress(token_id), chain=chain)
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
+
+        self.token_ids[other_chain].append(token_id)
+        t = IERC20(self.deployments[other_chain].its.validTokenAddress(token_id), chain=other_chain)
+        if t not in self.balances:
+            self.balances[t] = defaultdict(int)
+
+        if token_manager_type in {
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+        }:
+            self.balances[IERC20(source_token, chain=chain)][source_token_manager] += 10_000_000
+            self.balances[IERC20(dest_token, chain=other_chain)][dest_token_manager] += 10_000_000
 
     @flow()
     def flow_token_interchain_transfer(self):
@@ -296,12 +384,67 @@ class ItsFuzzTest(FuzzTest):
         tx = token.interchainTransfer(
             f"chain{other_chain.chain_id}", bytes(recipient), amount, b"", from_=sender
         )
+
+        # source token manager is mint-burn
+
         dest_its = self.deployments[other_chain].its
         dest_token = IERC20(dest_its.validTokenAddress(token_id), chain=other_chain)
+        dest_token_manager = TokenManagerProxy(dest_its.tokenManagerAddress(token_id), chain=other_chain)
+        if dest_token_manager.implementationType() in {
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+        }:
+            self.balances[dest_token][dest_token_manager] -= amount
+        elif dest_token_manager.implementationType() == InterchainTokenService.TokenManagerType.GATEWAY:
+            self.balances[dest_token][self.deployments[other_chain].gw] -= amount
 
-        # TODO fee on transfer
-        #if token.symbol() == "STA":
-            #breakpoint()
+        if TokenManagerProxy(dest_its.tokenManagerAddress(token_id), chain=other_chain).implementationType() == InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE:
+            amount -= IStatera(dest_token, chain=other_chain).cut(amount)
+
+        self.balances[dest_token][Account(recipient, chain=other_chain)] += amount
+
+        logger.info(f"Transferred {amount} {token.symbol()} from {chain.chain_id} to {other_chain.chain_id}")
+
+    @flow()
+    def flow_token_interchain_transfer_from(self):
+        chain = random.choice([chain1, chain2])
+        other_chain = chain1 if chain == chain2 else chain2
+        if len(self.interchain_tokens[chain]) == 0:
+            return
+
+        token = random.choice(self.interchain_tokens[chain])
+        token_id = token.interchainTokenId()
+        owner = random_account(chain=chain)
+        spender = random_account(chain=chain)
+        recipient = random_address()
+        amount = random_int(0, 1_000, edge_values_prob=0.15)
+        mint_erc20(token, owner, amount)
+        token.approve(spender, amount, from_=owner)
+
+        dest_its = self.deployments[other_chain].its
+        dest_token = IERC20(dest_its.validTokenAddress(token_id), chain=other_chain)
+        dest_token_manager = TokenManagerProxy(dest_its.tokenManagerAddress(token_id), chain=other_chain)
+
+        tx = token.interchainTransferFrom(
+            owner, f"chain{other_chain.chain_id}", bytes(recipient), amount, b"", from_=spender
+        )
+
+        # source token manager is mint-burn
+
+        dest_its = self.deployments[other_chain].its
+        dest_token = IERC20(dest_its.validTokenAddress(token_id), chain=other_chain)
+        dest_token_manager = TokenManagerProxy(dest_its.tokenManagerAddress(token_id), chain=other_chain)
+        if dest_token_manager.implementationType() in {
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+            InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+        }:
+            self.balances[dest_token][dest_token_manager] -= amount
+        elif dest_token_manager.implementationType() == InterchainTokenService.TokenManagerType.GATEWAY:
+            self.balances[dest_token][self.deployments[other_chain].gw] -= amount
+
+        if dest_token_manager.implementationType() == InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE:
+            amount -= IStatera(dest_token, chain=other_chain).cut(amount)
+
         self.balances[dest_token][Account(recipient, chain=other_chain)] += amount
 
         logger.info(f"Transferred {amount} {token.symbol()} from {chain.chain_id} to {other_chain.chain_id}")
@@ -317,6 +460,7 @@ class ItsFuzzTest(FuzzTest):
         amount = random_int(0, 1_000, edge_values_prob=0.15)
 
         token = IERC20Named(its.validTokenAddress(token_id), chain=source_chain)
+        token_manager = TokenManagerProxy(its.tokenManagerAddress(token_id), chain=source_chain)
         mint_erc20(token, sender, amount)
 
         if amount > 0:
@@ -333,12 +477,31 @@ class ItsFuzzTest(FuzzTest):
                 from_=sender,
             )
 
+            if token_manager.implementationType() == InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE:
+                amount -= IStatera(token, chain=source_chain).cut(amount)
+
+            if token_manager.implementationType() in {
+                InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+                InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+            }:
+                self.balances[token][token_manager] += amount
+            elif token_manager.implementationType() == InterchainTokenService.TokenManagerType.GATEWAY:
+                self.balances[token][self.deployments[source_chain].gw] += amount
+
             dest_its = self.deployments[dest_chain].its
             dest_token = IERC20(dest_its.validTokenAddress(token_id), chain=dest_chain)
+            dest_token_manager = TokenManagerProxy(dest_its.tokenManagerAddress(token_id), chain=dest_chain)
+            if dest_token_manager.implementationType() in {
+                InterchainTokenService.TokenManagerType.LOCK_UNLOCK,
+                InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE,
+            }:
+                self.balances[dest_token][dest_token_manager] -= amount
+            elif dest_token_manager.implementationType() == InterchainTokenService.TokenManagerType.GATEWAY:
+                self.balances[dest_token][self.deployments[dest_chain].gw] -= amount
 
-            # TODO fee on transfer
-            #if token.symbol() == "STA":
-                #breakpoint()
+            if dest_token_manager.implementationType() == InterchainTokenService.TokenManagerType.LOCK_UNLOCK_FEE:
+                amount -= IStatera(dest_token, chain=dest_chain).cut(amount)
+
             self.balances[dest_token][Account(recipient, chain=dest_chain)] += amount
 
             logger.info(f"Transferred {amount} {token.symbol()} from {source_chain.chain_id} to {dest_chain.chain_id}")
@@ -355,8 +518,8 @@ class ItsFuzzTest(FuzzTest):
                     assert token.balanceOf(a) == b
 
 
-@chain1.connect(chain_id=1, fork="http://localhost:8545")
-@chain2.connect(chain_id=2, fork="http://localhost:8545")
+@chain1.connect(chain_id=1, fork="http://localhost:8545@19378463")
+@chain2.connect(chain_id=2, fork="http://localhost:8545@19378463")
 @on_revert(lambda e: print(e.tx.call_trace if e.tx else "Call reverted"))
 def test_its_fuzz():
     ItsFuzzTest().run(10, 10_000)
